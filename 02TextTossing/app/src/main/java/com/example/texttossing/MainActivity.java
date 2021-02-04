8ce623758a6aa6de1eda5e34b47840b74b480e54
gwit13 <gwit0513@gmail.com>
2021-02-04 16:00:34 -0500

    Complete basic working app

--- 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
+++ 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.texttossing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button confirm;
+    TextView display;
+    EditText input;
+    String temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        confirm = findViewById(R.id.confirm_button);
+        display = findViewById(R.id.display);
+        input = findViewById(R.id.text_input_1);
+    }
+
+    public void nameConfirm(View view) {
+        temp = input.getText().toString();
+        if(temp.length() > 0) {
+            input.setText("");
+            display.setText("Hello, " + temp + " !");
+        }
     }
 }
