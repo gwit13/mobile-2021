91cf6f2de9a1bd7608bb01efe953d5d2a2160b53
gwit13 <gwit0513@gmail.com>
2021-02-09 15:46:47 -0500

    Finish Part 1, cycle through and update textfields from input

--- 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
+++ 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.texttossing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,23 +11,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button confirm;
-    TextView display;
+    TextView name;
+    TextView graduatingClass;
+    TextView favoriteMonkey;
+    TextView[] views = new TextView[3];
     EditText input;
     String temp;
+    int field;
+    int color;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         confirm = findViewById(R.id.confirm_button);
-        display = findViewById(R.id.display);
+        name = findViewById(R.id.display);
+        graduatingClass = findViewById(R.id.graduatingclass);
+        favoriteMonkey = findViewById(R.id.favoritemonkey);
         input = findViewById(R.id.text_input_1);
+        views[0] = name;
+        views[1] = graduatingClass;
+        views[2] = favoriteMonkey;
+        field = 0;
+        color = name.getTextColors().getDefaultColor();
+        name.setTextColor(Color.RED);
     }
 
     public void nameConfirm(View view) {
         temp = input.getText().toString();
         if(temp.length() > 0) {
             input.setText("");
-            display.setText("Hello, " + temp + " !");
+            views[field].setText("" + temp);
+            views[field].setTextColor(color);
+            field = (++field)%3;
+            views[field].setTextColor(Color.RED);
         }
     }
 }
