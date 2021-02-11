7e16a2f2f88e3767389597393f69e988c67f46ec
gwit13 <gwit0513@gmail.com>
2021-02-10 23:21:16 -0500

    Partially implement mutable preset list. Adding own presets coming soon!

--- 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
+++ 02TextTossing/app/src/main/java/com/example/texttossing/MainActivity.java
@@ -10,14 +10,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Resources res;
     Button confirm;
+    ArrayList<String> namesMut = new ArrayList<>();
+    ArrayList<String> classesMut = new ArrayList<>();
+    ArrayList<String> monkeysMut = new ArrayList<>();
+
     TextView name;
     TextView graduatingClass;
     TextView favoriteMonkey;
     TextView[] views = new TextView[3];
     EditText input;
+
     String temp;
     int fieldInput;
     int presetInterator;
@@ -27,17 +34,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         res = getResources();
+
         confirm = findViewById(R.id.confirm_button);
         name = findViewById(R.id.display);
         graduatingClass = findViewById(R.id.graduatingclass);
         favoriteMonkey = findViewById(R.id.favoritemonkey);
         input = findViewById(R.id.text_input_1);
+
+        for(int i = 0; i < res.getStringArray(R.array.names).length; i++) {
+            namesMut.add(res.getStringArray(R.array.names)[i]);
+            classesMut.add(res.getStringArray(R.array.names)[i]);
+            monkeysMut.add(res.getStringArray(R.array.names)[i]);
+        }
+
         views[0] = name;
         views[1] = graduatingClass;
         views[2] = favoriteMonkey;
         fieldInput = 0;
         presetInterator = 0;
         color = name.getTextColors().getDefaultColor();
+
         name.setTextColor(Color.RED);
     }
 
@@ -59,5 +75,9 @@ public class MainActivity extends AppCompatActivity {
         views[1].setText(res.getStringArray(R.array.classes)[presetInterator]);
         views[2].setText(res.getStringArray(R.array.monkeys)[presetInterator]);
         presetInterator = (++presetInterator%(res.getStringArray(R.array.names).length));
+    }
+
+    public void save(View view) {
+        
     }
 }
