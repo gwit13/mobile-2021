4ea5db59c85f4d46aadc3cb7eb4c836e6ec48e57
gwit13 <gwit0513@gmail.com>
2021-02-13 23:01:40 -0500

    Fix actual java file not being pushed to git oops

---
+++ 03PreferenceSharingToasting/app/src/main/java/com/example/preferencesharingtoasting/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.preferencesharingtoasting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void green(View view) {
+
+    }
+
+    public void red(View view) {
+
+    }
+
+    public void yellow(View view) {
+
+    }
+
+    public void blue(View view) {
+
+    }
+}
