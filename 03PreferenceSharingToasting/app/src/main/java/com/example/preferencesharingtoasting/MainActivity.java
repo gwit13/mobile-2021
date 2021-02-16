294dbb2a5fd85868d81515b99a5175931ecffe11
gwit13 <gwit0513@gmail.com>
2021-02-16 15:08:12 -0500

    Implement sharedPreferences

--- 03PreferenceSharingToasting/app/src/main/java/com/example/preferencesharingtoasting/MainActivity.java
+++ 03PreferenceSharingToasting/app/src/main/java/com/example/preferencesharingtoasting/MainActivity.java
@@ -2,30 +2,57 @@ package com.example.preferencesharingtoasting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    final int toastDuration = Toast.LENGTH_SHORT;
+    Toast toast;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    TextView green, red, yellow, blue, temp;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+        sharedPreferences = getSharedPreferences("Counters", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        green = findViewById(R.id.view1);
+        red = findViewById(R.id.view2);
+        yellow = findViewById(R.id.view3);
+        blue = findViewById(R.id.view4);
+        green.setOnClickListener(this);
+        red.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        blue.setOnClickListener(this);
+        replaceValues();
     }
 
-    public void green(View view) {
-
+    public void onClick(View v) {
+        temp = (TextView)v;
+        temp.setText("" + (Integer.parseInt((String) temp.getText()) + 1));
+//        toast.makeText(context, (temp.getId()+temp.getText()), toastDuration);
+        updateValues();
     }
 
-    public void red(View view) {
-
+    private void updateValues() {
+        editor.putInt("green", Integer.parseInt((String) green.getText()));
+        editor.putInt("red", Integer.parseInt((String) red.getText()));
+        editor.putInt("yellow", Integer.parseInt((String) yellow.getText()));
+        editor.putInt("blue", Integer.parseInt((String) blue.getText()));
+        editor.apply();
     }
 
-    public void yellow(View view) {
-
-    }
-
-    public void blue(View view) {
-
+    private void replaceValues() {
+        green.setText("" + sharedPreferences.getInt("green", 0));
+        red.setText("" + sharedPreferences.getInt("red", 0));
+        yellow.setText("" + sharedPreferences.getInt("yellow", 0));
+        blue.setText("" + sharedPreferences.getInt("blue", 0));
     }
 }
