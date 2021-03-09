34f093c22b978304d3fbadecbd55852e2785bbfb
gwit13 <gwit0513@gmail.com>
2021-03-09 15:34:12 -0500

    Implement SharedPreferences, finalize functionality, yes I am doing better now.

--- 05ActivityStateRecording/app/src/main/java/com/example/activitystaterecording/MainActivity.java
+++ 05ActivityStateRecording/app/src/main/java/com/example/activitystaterecording/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.activitystaterecording;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView create, start, resume, pause, stop, restart, destroy;
     int createi, starti, resumei, pausei, stopi, restarti, destroyi = 0;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,42 +26,74 @@ public class MainActivity extends AppCompatActivity {
         restart = findViewById(R.id.onRestartView);
         destroy = findViewById(R.id.onDestroyView);
 
-        create.setText("onCreate: called " + ++createi + " times.");
-        start.setText("onStart: called 0 times.");
-        resume.setText("onResume: called 0 times.");
-        pause.setText("onPause: called 0 times.");
-        stop.setText("onStop: called 0 times.");
-        restart.setText("onRestart: called 0 times.");
-        destroy.setText("onDestroy: called 0 times.");
+        sharedPreferences = getSharedPreferences("Counters", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        if(sharedPreferences.contains("create"))
+            editor.putInt("create", (sharedPreferences.getInt("create", 0)) + 1);
+        else
+            editor.putInt("create", 1);
+
+        editor.apply();
+        createi++;
+        print();
     }
     @Override
     protected void onStart() {
         super.onStart();
-        start.setText("onStart: called " + ++starti + " times.");
+        starti++;
+        editor.putInt("start", (sharedPreferences.getInt("start", 0)) + 1);
+        editor.apply();
+        print();
     }
 
     protected void onResume() {
         super.onResume();
-        resume.setText("onResume: called " + ++resumei + " times.");
+        resumei++;
+        editor.putInt("resume", (sharedPreferences.getInt("resume", 0)) + 1);
+        editor.apply();
+        print();
     }
 
     protected void onPause() {
         super.onPause();
-        pause.setText("onPause: called " + ++pausei + " times.");
+        pausei++;
+        editor.putInt("pause", (sharedPreferences.getInt("pause", 0)) + 1);
+        editor.apply();
+        print();
     }
 
     protected void onStop() {
         super.onStop();
-        stop.setText("onPause: called " + ++stopi + " times.");
+        stopi++;
+        editor.putInt("stop", (sharedPreferences.getInt("stop", 0)) + 1);
+        editor.apply();
+        print();
     }
 
     protected void onRestart() {
         super.onRestart();
-        restart.setText("onRestart: called " + ++restarti + " times.");
+        restarti++;
+        editor.putInt("restart", (sharedPreferences.getInt("restart", 0)) + 1);
+        editor.apply();
+        print();
     }
 
     protected void onDestroy() {
         super.onDestroy();
-        destroy.setText("onDestroy: called " + ++destroyi + " times.");
+        destroyi++;
+        editor.putInt("destroy", (sharedPreferences.getInt("destroy", 0)) + 1);
+        editor.apply();
+        print();
+    }
+
+    private void print() {
+        create.setText("onCreate: called " + createi + " times session, " + sharedPreferences.getInt("create", 0) + " total.");
+        start.setText("onStart: called " + starti + " times session, " + sharedPreferences.getInt("start", 0) + " total.");
+        resume.setText("onResume: called " + resumei + " times session, " + sharedPreferences.getInt("resume", 0) + " total.");
+        pause.setText("onPause: called " + pausei + " times session, " + sharedPreferences.getInt("pause", 0) + " total.");
+        stop.setText("onStop: called " + stopi + " times session, " + sharedPreferences.getInt("stop", 0) + " total.");
+        restart.setText("onRestart: called " + restarti + " times session, " + sharedPreferences.getInt("restart", 0) + " total.");
+        destroy.setText("onDestroy: called " + destroyi + " times session, " + sharedPreferences.getInt("destroy", 0) + " total.");
     }
 }
